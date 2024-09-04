319de23d81ae8d4dfe2c64949b403c5a36a02751
Carlos Gómez <carlosgomez.deb@gmail.com>
2024-09-03 19:57:33 -0600

    :sparkles: - included missing crud methods and tags for swagger

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.post("/token", response_model=schemas.Token)
+@app.post("/token", response_model=schemas.Token, tags=["Authentication"])
 async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
     user = auth.authenticate_user(db, form_data.username, form_data.password)
     if not user:
@@ -35,14 +35,42 @@ async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
-@app.post("/users/", response_model=schemas.User)
+# GET /users/
+@app.post("/users/", response_model=schemas.User, tags=["Users"])
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
     return crud.create_user(db=db, user=user)
 
-@app.get("/pronostico/{ciudad}", response_model=schemas.Pronostico)
+# GET /users/:id
+@app.get("/users/{user_id}", response_model=schemas.User, tags=["Users"])
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+# PATCH /users/:id
+@app.patch("/users/{user_id}", response_model=schemas.User, tags=["Users"])
+def update_user(user_id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
+    db_user = crud.update_user(db, user_id, user)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+# DELETE /users/:id
+@app.delete("/users/{user_id}", response_model=schemas.User, tags=["Users"])
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.delete_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+
+
+@app.get("/pronostico/{ciudad}", response_model=schemas.Pronostico, tags=["Pronostico"])
 async def get_pronostico(ciudad: str, db: Session = Depends(get_db), current_user: schemas.User = Depends(auth.get_current_user)):
     pronostico, temperatura = obtener_pronostico(ciudad)
     weather_query = schemas.WeatherQueryCreate(
