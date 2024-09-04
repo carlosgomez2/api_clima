ad454059a5b53da1be4e2c93891db250b3a38787
Carlos Gómez <carlosgomez.deb@gmail.com>
2024-09-04 13:53:01 -0600

    :-1: - sqlalchemy deprecation

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base # Deprecated
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+
+from sqlalchemy.orm import declarative_base
 
 load_dotenv()
 
